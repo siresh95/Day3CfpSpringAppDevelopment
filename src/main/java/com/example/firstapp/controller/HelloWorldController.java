e44f77a15121c4553aac75699d2cd64334a454bc
Resh@308 <reshsingh1995@gmail.com>
2022-06-01 02:08:30 +0530

    first name last name added

--- src/main/java/com/example/firstapp/controller/HelloWorldController.java
+++ src/main/java/com/example/firstapp/controller/HelloWorldController.java
@@ -1,13 +1,24 @@
 package com.example.firstapp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
+
 @RestController
-@RequestMapping("/helloController")
+@RequestMapping("/hello")
 public class HelloWorldController {
-    @RequestMapping(value = {"", "/" ,"/home"})
-    public String sayHello(){
-        return "Hello World ! ! !";
+    @RequestMapping(value = {"", "/", "/home"})
+    public String sayHello() {
+        return "Hello from bridgelabz";}
+        @RequestMapping(value ={"/query"}, method = RequestMethod.GET)
+        public String sayHello(@RequestParam(value = "fname") String fname, @RequestParam(value = "lname") String lname)
+        {
+            return "Hello" + fname +  lname + "!";
+        }
     }
-}
+
+
